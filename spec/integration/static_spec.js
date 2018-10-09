5ad0ea08147d22508636f11b381e5d06a166f71d
Derek Lundahl <dereklundahl@yahoo.com>
2018-10-08 21:06:18 -0600

    added the test code

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,21 +4,30 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
 
-//#1
   describe("GET /", () => {
 
-//#2
     it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
 
-//#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
 
-//#4
         done();
       });
     });
-
   });
+
+  describe("GET /about", () => {
+
+    it("should return status code 200 and have 'About Us' in the body of the response", () => {
+
+      request.get(base + "about", (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
+
+        done();
+      });
+    });
+  });
+  
 });
