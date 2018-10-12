0e0ad13230a2b68789d136ac2ba9d568ed3b076b
Derek Lundahl <dereklundahl@yahoo.com>
2018-10-11 20:55:11 -0600

    pushing to get help on slack

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,7 +6,7 @@ describe("routes : static", () => {
 
   describe("GET /", () => {
 
-    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
@@ -19,13 +19,14 @@ describe("routes : static", () => {
 
   describe("GET /about", () => {
 
-    it("should return status code 200 and have 'About Us' in the body of the response", () => {
+    it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
 
       request.get(base + "about", (err, res, body) => {
+        console.log("BODY:", body);
         expect(res.statusCode).toBe(200);
         expect(body).toContain("About Us");
 
-        //done();
+        done();
       });
     });
   });
