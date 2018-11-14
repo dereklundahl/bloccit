0ef3df58c6a8cba1054390669e3c4a610fdbada2
Derek Lundahl <dereklundahl@yahoo.com>
2018-11-14 09:57:53 -0700

    removed syntax causing errors after merge

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -4,15 +4,7 @@ module.exports = {
   },
 
   about(req, res, next) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    res.render("static/about", { h1: 'About Us' });
-=======
+    
     res.render("static/about", {h1: "About Us"});
->>>>>>> assignment-4
-=======
-
-    res.render("static/about", {h1: "About Us"});
->>>>>>> 2caaf9f9ed0df2c72a8a931095e5bf571b3f9228
   }
 }
