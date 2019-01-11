125c4f11d594d246cae9a3efebee2a4164e9fcb5
Derek Lundahl <dereklundahl@yahoo.com>
2019-01-11 13:48:02 -0700

    unit tests passing now

--- spec/unit/comment_spec.js
+++ spec/unit/comment_spec.js
@@ -6,164 +6,178 @@ const Comment = require("../../src/db/models").Comment;
 
 describe("Comment", () => {
 
-    beforeEach((done) => {
-        this.user;
-        this.topic;
-        this.post;
-        this.comment;
+  beforeEach((done) => {
+    this.user;
+    this.topic;
+    this.post;
+    this.comment;
 
-        sequelize.sync({force: true}).then((res) => {
+    sequelize.sync({force: true}).then((res) => {
 
-            User.create({
-                email: "starman@tesla.com",
-                password: "Trekkie4lyfe"
-            })
-            .then((user) => {
-                this.user = user;
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
+      })
+      .then((user) => {
+        this.user = user;
 
-                Topic.create({
-                    title: "Expeditions to Alpha Centauri",
-                    description: "A compilation of reports from recent visits to the star system",
-                    posts: [{
-                        title: "My first visit to Proxima Centauri b",
-                        body: "I saw some rocks.",
-                        userId: this.user.id
-                    }]
-                }, {
-                    include: {
-                        model: Post,
-                        as: "posts"
-                    }
-                })
-                .then((topic) => {
-                    this.topic = topic;
-                    this.post = this.topic.post[0];
+        Topic.create({
+          title: "Expeditions to Alpha Centauri",
+          description: "A compilation of reports from recent visits to the star system.",
+          posts: [{
+            title: "My first visit to Proxima Centauri b",
+            body: "I saw some rocks.",
+            userId: this.user.id
+          }]
+        }, {
+          include: {
+            model: Post,
+            as: "posts"
+          }
+        })
+        .then((topic) => {
+          this.topic = topic;
+          this.post = this.topic.posts[0];
 
-                    Comment.create({
-                        body: "ay caramba!!!!!",
-                        userId: this.user.id,
-                        postId: this.post.id
-                    })
-                    .then((comment) => {
-                        this.comment = comment;
-                        done();
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                        done();
-                    });
-                });
-            });
+          Comment.create({
+            body: "ay caramba!!!!!",
+            userId: this.user.id,
+            postId: this.post.id
+          })
+          .then((comment) => {
+            this.comment = comment;
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
         });
-        
-        describe("#create()", () => {
+      });
+    });
+  });
 
-            it("should create a comment object with a body, assigned post and user", (done) => {
-                Comment.create({
-                    body: "The geological kind.",
-                    postId: this.post.id,
-                    userId: this.user.id
-                })
-                .then((comment) => {
-                    expect(comment.body).toBe("The geoligical kind.");
-                    expect(comment.postId).toBe(this.post.id);
-                    expect(comment.userId).toBe(this.user.id);
-                    done();
-                })
-                .catch((err) => {
-                    console.log(err);
-                    done();
-                });
-            });
+  describe("#create()", () => {
 
-            it("should not create a comment with missing body, assigned post or user", (done) => {
-                Comment.create({
-                    body: "Are the inertial dampers still engaged?"
-                })
-                .then((comment) => {
-                    done();
-                })
-                .catch((err) => {
+    it("should create a comment object with a body, assigned post and user", (done) => {
+      Comment.create({                
+        body: "The geological kind.",
+        postId: this.post.id,
+        userId: this.user.id
+      })
+      .then((comment) => {            
+        expect(comment.body).toBe("The geological kind.");
+        expect(comment.postId).toBe(this.post.id);
+        expect(comment.userId).toBe(this.user.id)
+        done();
 
-                    expect(err.message).toContain("Comment.userId cannot be null");
-                    expect(err.message).toContain("Comment.postId cannot be null");
-                    done();
-                })
-            });
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
+
+
+    it("should not create a comment with missing body, assigned post or user", (done) => {
+      Comment.create({
+        body: "Are the inertial dampers still engaged?"
+      })
+      .then((comment) => {
+        // the code in this block will not be evaluated since the validation error
+        done();
+
+      })
+      .catch((err) => {
+
+        expect(err.message).toContain("Comment.userId cannot be null");
+        expect(err.message).toContain("Comment.postId cannot be null");
+        done();
+
+      })
+    });
+
+  });
+
+  describe("#setUser()", () => {
+
+    it("should associate a comment and a user together", (done) => {
+
+      User.create({             
+        email: "bob@example.com",
+        password: "password"
+      })
+      .then((newUser) => {        
+
+        expect(this.comment.userId).toBe(this.user.id);
+
+        this.comment.setUser(newUser)                   
+        .then((comment) => {
+
+          expect(comment.userId).toBe(newUser.id);      
+          done();
 
         });
+      })
+    });
 
-        describe("#setUser()", () => {
-            
-            it("should associate a comment and a user together", (done) => {
+  });
 
-                User.create({
-                    email: "bob@example.com",
-                    password: "password"
-                })
-                .then((newUser) => {
-                    
-                    expect(this.comment.userId).toBe(this.user.id);
+  describe("#getUser()", () => {
 
-                    this.comment.setUser(newUser)
-                    .then((comment) => {
+    it("should return the associated user", (done) => {
 
-                        expect(comment.userId).toBe(newUser.id);
-                        done();
+      this.comment.getUser()
+      .then((associatedUser) => {
+        expect(associatedUser.email).toBe("starman@tesla.com");
+        done();
+      });
 
-                    });
-                })
-            });
+    });
+
+  });
+
+  describe("#setPost()", () => {
+
+    it("should associate a post and a comment together", (done) => {
+
+      Post.create({       
+        title: "Dress code on Proxima b",
+        body: "Spacesuit, space helmet, space boots, and space gloves",
+        topicId: this.topic.id,
+        userId: this.user.id
+      })
+      .then((newPost) => {
+
+        expect(this.comment.postId).toBe(this.post.id);
+
+        this.comment.setPost(newPost)                  
+        .then((comment) => {
+
+          expect(comment.postId).toBe(newPost.id);      
+          done();
+
         });
+      })
+    });
 
-        describe("#getUser()", () => {
+  });
 
-            it("should return the associated user", (done) => {
+  describe("#getPost()", () => {
 
-                this.comment.getUser()
-                .then((associatedUser) => {
-                    expect(associatedUser.email).toBe("starman@tesla.com");
-                    done();
-                });
-            });
-        });
+    it("should return the associated post", (done) => {
 
-        describe("#setPost()", () => {
+      this.comment.getPost()
+      .then((associatedPost) => {
+        expect(associatedPost.title).toBe("My first visit to Proxima Centauri b");
+        done();
+      });
 
-            it("should associate a post and a comment together", (done) => {
+    });
 
-                Post.create({
-                    title: "Dress code on Proxima b",
-                    body: "Spacesuit, space helmet, space boots, and space gloves",
-                    topicId: this.topic.id,
-                    userId: this.user.id
-                })
-                .then((newPost) => {
-
-                    expect(this.comment.postId).toBe(this.post.id);
-
-                    this.comment.setPost(newPost)
-                    .then((comment) => {
-
-                        expect(comment.postId).toBe(newPost.id);
-                        done();
-                    });
-                })
-            });
-        });
-
-        describe("#getPost()", () => {
-
-            it("should return the associated post", (done) => {
-
-                this.comment.getPost()
-                .then((associatedPost) => {
-                    expect(associatedPost.title).toBe("My first visit to Proxima Centauri b");
-                    done();
-                });
-            });
-        });
-
-    }); 
-    
-});   
+  });
+});
